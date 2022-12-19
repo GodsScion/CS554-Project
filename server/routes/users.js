c8613f70f1ba05f41d3d8ff0cf3d9b958e719d6f
Nap Cracker <viveksharma.ponnekanti@gmail.com>
2022-12-19 17:12:41 -0500

    User status API

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,6 +5,7 @@ const router = require("express").Router();
 router.post("/login", users.login);
 router.post("/logout", users.logout);
 router.post("/signup", users.signUp);
+router.get("/status", users.getUserStatus);
 router.get("/:id", users.getUser);
 
 module.exports = router;
