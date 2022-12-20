4388962f42a550afc3c3ee14e169ef20113eb73f
Chendra Kousik Jillellamudi <100979774+kousikchendra@users.noreply.github.com>
2022-12-20 14:59:24 -0500

    Update ChatRoom.js

--- client/src/components/discussions/ChatRoom.js
+++ client/src/components/discussions/ChatRoom.js
@@ -8,7 +8,7 @@ function ChatRoom() {
   const location = useLocation();
   var userName = "";
   if (location.state === null) {
-    userName = "Anonymous"
+    userName = "Anonymous";
   } else {
     const { username } = location.state;
     userName = username;
@@ -19,39 +19,43 @@ function ChatRoom() {
   const handleNewMessageChange = (event) => {
     setNewMessage(event.target.value);
   };
- 
+
   const handleSendMessage = () => {
     sendMessage(newMessage);
     setNewMessage("");
   };
   return (
     <div className="chat-room-container">
-    <h1 className="room-name">Room: {roomId}, Joined as: {userName}</h1>
-    <div className="messages-container">
-      <ol className="messages-list">
-        {messages.map((message, i) => (
-          <li
-            key={i}
-            className={`message-item ${
-              message.ownedByCurrentUser ? "my-message" : "received-message"
-            }`}
-          >
-          {message.userName}:{message.body}
-          </li>
-        ))}
-      </ol>
+      <h1 className="room-name">
+        Room: {roomId}, Joined as: {userName}
+      </h1>
+      <div className="messages-container">
+        <ol className="messages-list">
+          {messages.map((message, i) => (
+            <li
+              key={i}
+              className={`message-item ${
+                message.ownedByCurrentUser ? "my-message" : "received-message"
+              }`}
+            >
+              {message.userName}:{message.body}
+            </li>
+          ))}
+        </ol>
+      </div>
+      {/* <p>{userName}</p> */}
+      <label htmlFor="my-input3"></label>
+      <textarea
+        id="my-input3"
+        value={newMessage}
+        onChange={handleNewMessageChange}
+        placeholder="Write message..."
+        className="new-message-input-field"
+      />
+      <button onClick={handleSendMessage} className="send-message-button">
+        Send
+      </button>
     </div>
-    {/* <p>{userName}</p> */}
-    <textarea
-      value={newMessage}
-      onChange={handleNewMessageChange}
-      placeholder="Write message..."
-      className="new-message-input-field"
-    />
-    <button onClick={handleSendMessage} className="send-message-button">
-      Send
-    </button>
-  </div>
   );
 }
 
