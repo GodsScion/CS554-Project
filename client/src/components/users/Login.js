cb1cc8d0272d199c3d13f6c7bb68c27c18a0789c
GodsScion <100998531+GodsScion@users.noreply.github.com>
2022-12-20 00:27:33 -0500

    Login and signup pages

--- client/src/components/users/Login.js
+++ client/src/components/users/Login.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+const xss = require('xss');
 
 const Login = () => {
+    const navigate = useNavigate()
+
+    async function hangleLogin(e){
+        e.preventDefault();
+
+console.log(e)
+    }
+
     return(
-        <div className="container m-5 p-2 bg-body rounded">
+            <div className="card position-absolute top-50 start-50 translate-middle p-3">
+            <div className="card-body" >
+                <h2 className="card-title">Please enter your details</h2>
+                <form>
+                    <label htmlFor='email'>Email: </label>
+                    <input id="email" name="email" type="email" className='form-control mb-3' placeholder='email' required></input>
+                    <label htmlFor='password'>Email: </label>
+                    <input id="password" name="password" type="password" className='form-control mb-3' placeholder='password' required></input>
+                    <div className='form-text'>email and password are required</div>
+                    <br/>
+                    <button className='btn btn-primary' onClick={hangleLogin}>Login</button>
+                </form>
+            </div>
+            <button className='btn btn-primary' onClick={()=>{navigate('/register')}}>Sign Up</button>
+            </div>
             
-        </div>
     )
 }
 
--- client/src/components/users/Register.js
+++ client/src/components/users/Register.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+const xss = require('xss');
 
 const Register = () => {
-    return(
-        <div className="container m-5 p-2 bg-body rounded">
-            
+    const navigate = useNavigate();
 
+    return(
+        <div className="container position-absolute top-50 start-50 translate-middle m-5 p-2 bg-body rounded">
+            <div className="card">
+            <div className="card-body" >
+                <h2 className="card-title">Please enter your details</h2>
+                <form>
+                    <input id="name" name="name" className='form-control' placeholder='How would you like to be called?'>xss()</input>
+
+                </form>
+            </div>
+            </div>
+            <hr/>
+            <p>Already have an account? Click Login to login</p>
+            <button className='btn btn-primary' onClick={()=>{navigate('/login')}}>Login</button>
         </div>
     )
 }
