622a042105e0115b708365ecd30cd96f7a111969
Nap Cracker <viveksharma.ponnekanti@gmail.com>
2022-12-20 21:27:34 -0500

    Password Changes

--- server/validators/users.js
+++ server/validators/users.js
@@ -1,4 +1,5 @@
 const Joi = require("joi");
+const passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
 
 module.exports = {
   validateSignUp,
@@ -17,7 +18,7 @@ function validateSignUp(requestBody) {
     firstName: Joi.string().alphanum().min(3).max(20).required(),
     lastName: Joi.string().alphanum().min(1).max(20).required(),
     email: Joi.string().email().required(),
-    password: Joi.string().required(),
+    password: Joi.string().regex(passwordRegex).required(),
     img: Joi.string().required(),
   });
   const { error } = schema.validate(requestBody);
@@ -28,7 +29,7 @@ function validateSignUp(requestBody) {
 function validateLogin(requestBody) {
   const schema = Joi.object().keys({
     email: Joi.string().email().required(),
-    password: Joi.string().required(),
+    password: Joi.string().regex(passwordRegex).required(),
   });
   const { error } = schema.validate(requestBody);
   if (error) return { isInvalid: true, message: error.message };
@@ -39,9 +40,9 @@ function validateEditUser(requestBody) {
   const schema = Joi.object().keys({
     firstName: Joi.string().alphanum().min(3).max(20).required(),
     lastName: Joi.string().alphanum().min(1).max(20).required(),
-    oldPassword: Joi.string().required(),
+    oldPassword: Joi.string().regex(passwordRegex).required(),
     newPassword: Joi.string().required(),
-    img: Joi.string().required(),
+    img: Joi.string().regex(passwordRegex).required(),
   });
   const { error } = schema.validate(requestBody);
   if (error) return { isInvalid: true, message: error.message };
